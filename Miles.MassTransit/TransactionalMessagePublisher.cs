929e6775fb39bf6a678f059db3d6fd1218786cf1
Adam Burton <adz21c@gmail.com>
2016-07-09 19:35:15 +0100

    Execute message handlers pre-commit.

--- Miles.MassTransit/TransactionalMessagePublisher.cs
+++ Miles.MassTransit/TransactionalMessagePublisher.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Miles.MassTransit
 {
@@ -24,7 +25,7 @@ namespace Miles.MassTransit
         private HashSet<OutgoingMessageAndObject> pendingSaveMessages = new HashSet<OutgoingMessageAndObject>();
         private HashSet<OutgoingMessageAndObject> pendingDispatchMessages = new HashSet<OutgoingMessageAndObject>();
         // handlers
-        private readonly Dictionary<Type, List<object>> immediateMessageHandlers = new Dictionary<Type, List<object>>();
+        private readonly Dictionary<Type, List<IObjectMessageProcessor>> immediateMessageHandlers = new Dictionary<Type, List<IObjectMessageProcessor>>();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="TransactionalMessagePublisher" /> class.
@@ -46,14 +47,25 @@ namespace Miles.MassTransit
             this.outgoingEventRepository = outgoingEventRepository;
             this.time = time;
 
-            var correlationId = consumeContext?.CorrelationId ?? NewId.NextGuid();
+            var correlationId = consumeContext?.CorrelationId ?? NewId.NextGuid();  // TODO, maybe want to centerlize this for easy logging etc
 
-            // Just before commit save all the outgoing messages and generate their ids.
             transaction.PreCommit.Register(async (s, e) =>
             {
+                // Just before commit save all the outgoing messages and generate their ids.
                 await outgoingEventRepository.SaveAsync(pendingSaveMessages.Select(x => x.GenerateOutgoingMessage(correlationId, time.Now))).ConfigureAwait(false);
                 pendingDispatchMessages = pendingSaveMessages;
                 pendingSaveMessages = new HashSet<OutgoingMessageAndObject>();
+
+                // Execute any immediate message handlers so they are within transaction
+                foreach (var message in pendingDispatchMessages)
+                {
+                    List<IObjectMessageProcessor> handlers;
+                    if (immediateMessageHandlers.TryGetValue(message.MessageType, out handlers))
+                    {
+                        foreach (var handler in handlers)
+                            await handler.Process(message);
+                    }
+                }
             });
 
             // After commit dispatch the messages. Try to mark them as dispatched.
@@ -108,18 +120,6 @@ namespace Miles.MassTransit
 
         #endregion
 
-        private void AddMessageHandler<TMessage>(IMessageProcessor<TMessage> messageHandler) where TMessage : class
-        {
-            var messageType = typeof(TMessage);
-            List<object> handlers;
-            if (!immediateMessageHandlers.TryGetValue(messageType, out handlers))
-            {
-                handlers = new List<object>();
-                immediateMessageHandlers.Add(messageType, handlers);
-            }
-            handlers.Add(messageHandler);
-        }
-
         // Internal structure to keep the message object and its db representation together
         private class OutgoingMessageAndObject
         {
@@ -144,5 +144,41 @@ namespace Miles.MassTransit
                 return OutgoingMessage;
             }
         }
+
+        #region Register and calling message handlers immediately
+
+        private void AddMessageHandler<TMessage>(IMessageProcessor<TMessage> messageHandler) where TMessage : class
+        {
+            var messageType = typeof(TMessage);
+            List<IObjectMessageProcessor> handlers;
+            if (!immediateMessageHandlers.TryGetValue(messageType, out handlers))
+            {
+                handlers = new List<IObjectMessageProcessor>();
+                immediateMessageHandlers.Add(messageType, handlers);
+            }
+            handlers.Add(new ObjectToGenericMessageProcessor<TMessage>(messageHandler));
+        }
+
+        private interface IObjectMessageProcessor
+        {
+            Task Process(object message);
+        }
+
+        private class ObjectToGenericMessageProcessor<TMessage> : IObjectMessageProcessor
+        {
+            private readonly IMessageProcessor<TMessage> processor;
+
+            public ObjectToGenericMessageProcessor(IMessageProcessor<TMessage> processor)
+            {
+                this.processor = processor;
+            }
+
+            public Task Process(object message)
+            {
+                return processor.ProcessAsync((TMessage)message);
+            }
+        }
+
+        #endregion
     }
 }
