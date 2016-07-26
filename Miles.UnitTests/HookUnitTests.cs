dce5975192017841701dc3f1a0f4545e18e7d2d4
Adam Burton <adz21c@gmail.com>
2016-07-27 00:43:09 +0100

    Initial unit tests.

---
+++ Miles.UnitTests/HookUnitTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace Miles.UnitTests
+{
+    [TestFixture]
+    public class HookUnitTests
+    {
+        [Test]
+        public void Register_RegisteredProcessor_NoProcessorsRegistered()
+        {
+            var hook = new Hook<object, EventArgs>();
+
+            bool executed = false;
+            hook.Register((s, e) =>
+            {
+                executed = true;
+                return Task.FromResult(0);
+            });
+            hook.ExecuteAsync(new object(), new EventArgs()).Wait();
+
+            Assert.That(executed, Is.True);
+        }
+
+        [Test]
+        public void Register_RegisteredProcessors_NoProcessorsRegistered()
+        {
+            var hook = new Hook<object, EventArgs>();
+
+            bool executedOne = false;
+            hook.Register((s, e) =>
+            {
+                executedOne = true;
+                return Task.FromResult(0);
+            });
+            bool executedTwo = false;
+            hook.Register((s, e) =>
+            {
+                executedTwo = true;
+                return Task.FromResult(0);
+            });
+
+            hook.ExecuteAsync(new object(), new EventArgs()).Wait();
+
+            Assert.That(executedOne, Is.True);
+            Assert.That(executedTwo, Is.True);
+        }
+
+        [Test]
+        public void Register_DuplicateNotRegistered_IfAlreadyRegistered()
+        {
+            var hook = new Hook<object, EventArgs>();
+            int calls = 0;
+            var handler = new Func<object, EventArgs, Task>((s, e) =>
+            {
+                ++calls;
+                return Task.FromResult(0);
+            });
+
+            hook.Register(handler);
+            hook.Register(handler);
+
+            hook.ExecuteAsync(new object(), new EventArgs()).Wait();
+
+            Assert.That(calls, Is.EqualTo(1));
+        }
+    }
+}
