da7a5eb7fad74249b4e77ee75fd15e4ecd3c20b7
Adam Burton <adz21c@gmail.com>
2016-09-26 23:28:03 +0100

    Some documentation for the new MassTransit middleware types.

--- Miles.MassTransit/Configuration/IRecordMessageDispatchConfigurator.cs
+++ Miles.MassTransit/Configuration/IRecordMessageDispatchConfigurator.cs
@@ -2,8 +2,16 @@
 
 namespace Miles.MassTransit.Configuration
 {
+    /// <summary>
+    /// Allows developer to configure the <see cref="RecordMessageDispatchFilter{TContext}"/> .
+    /// </summary>
     public interface IRecordMessageDispatchConfigurator
     {
+        /// <summary>
+        /// Provide the <see cref="IDispatchedRepository"/> instance for recording message dispatch. 
+        /// </summary>
+        /// <param name="repository">The repository.</param>
+        /// <returns></returns>
         IRecordMessageDispatchConfigurator UseDispatchedRepository(IDispatchedRepository repository);
     }
 }
--- Miles.MassTransit/Configuration/MessageDeduplicationExtensions.cs
+++ Miles.MassTransit/Configuration/MessageDeduplicationExtensions.cs
@@ -1,11 +1,27 @@
 ﻿using MassTransit;
 using MassTransit.ConsumeConfigurators;
+using Microsoft.Practices.ServiceLocation;
 using Miles.MassTransit.MessageDeduplication;
+using Miles.Persistence;
 
 namespace Miles.MassTransit.Configuration
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class MessageDeduplicationExtensions
     {
+        /// <summary>
+        /// Wraps message in a <see cref="ITransactionContext"/> in which the message is recorded to ensure it is processed only once.
+        /// On identifying a message as already processed the message is removed from the queue without doing any work.
+        /// </summary>
+        /// <remarks>
+        /// This assumes a container will have registered itself as an <see cref="IServiceLocator"/> payload to 
+        /// retrieve an <see cref="IConsumedRepository"/> instance that will work with the <see cref="ITransactionContext"/>.
+        /// </remarks>
+        /// <typeparam name="TConsumer">The type of the consumer.</typeparam>
+        /// <param name="configurator">The configurator.</param>
+        /// <returns></returns>
         public static IConsumerConfigurator<TConsumer> UseMessageDeduplication<TConsumer>(this IConsumerConfigurator<TConsumer> configurator)
             where TConsumer : class, IConsumer
         {
