6faf0e26acdaca749bfc8deb18dd7d7b947a004b
adz21c <adz21c@gmail.com>
2017-01-09 17:02:49 +0000

    Nuget configuration

--- Miles.MassTransit.Hosting/Properties/AssemblyInfo.cs
+++ Miles.MassTransit.Hosting/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Miles.MassTransit.Hosting")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MassTransit hosting extensions")]
 [assembly: AssemblyConfiguration("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
