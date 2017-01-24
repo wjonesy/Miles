e69ee0aa7a14707acf8e6f59d178774aefc04104
adz21c <adz21c@gmail.com>
2017-01-24 12:04:26 +0000

    Itinerary extensions for queue convention

---
+++ Miles.MassTransit/Courier/ItineraryBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit.Courier;
+using System;
+using System.Collections.Generic;
+
+namespace Miles.MassTransit.Courier
+{
+    public static class ItineraryBuilderExtensions
+    {
+        public static void AddActivity<TArguments>(this ItineraryBuilder builder, string name, Uri hostAddress)
+        {
+            builder.AddActivity(name, new Uri(hostAddress, typeof(TArguments).GenerateExecutionQueueName()));
+        }
+
+        public static void AddActivity<TArguments>(this ItineraryBuilder builder, string name, Uri hostAddress, object arguments)
+        {
+            builder.AddActivity(name, new Uri(hostAddress, typeof(TArguments).GenerateExecutionQueueName()), arguments);
+        }
+
+        public static void AddActivity<TArguments>(this ItineraryBuilder builder, string name, Uri hostAddress, IDictionary<string, object> arguments)
+        {
+            builder.AddActivity(name, new Uri(hostAddress, typeof(TArguments).GenerateExecutionQueueName()), arguments);
+        }
+
+        public static void AddActivity<TArguments>(this ItineraryBuilder builder, string name, Uri hostAddress, TArguments arguments)
+        {
+            builder.AddActivity(name, new Uri(hostAddress, typeof(TArguments).GenerateExecutionQueueName()), arguments);
+        }
+    }
+}
