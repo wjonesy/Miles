379a839fd57741cd979f4728c5a35cfc9efbaa05
adz21c <adz21c@gmail.com>
2017-01-27 09:36:24 +0000

    Add subscription handling based on conventions.

--- Miles.MassTransit/Courier/IRoutingSlipPlanner.cs
+++ Miles.MassTransit/Courier/IRoutingSlipPlanner.cs
@@ -1,9 +1,19 @@
-﻿using MassTransit.Courier;
+﻿using MassTransit;
+using MassTransit.Courier;
+using MassTransit.Courier.Contracts;
+using System;
+using System.Threading.Tasks;
 
 namespace Miles.MassTransit.Courier
 {
     public interface IRoutingSlipPlanner : ItineraryBuilder
     {
         void AddActivity<TArguments>(string name, TArguments arguments) where TArguments : class;
+
+        Task AddSubscription<TEvent>(RoutingSlipEvents events, Func<ISendEndpoint, Task> callback) where TEvent : class;
+
+        Task AddSubscription<TEvent>(RoutingSlipEvents events, RoutingSlipEventContents contents, Func<ISendEndpoint, Task> callback) where TEvent : class;
+
+        Task AddSubscription<TEvent>(RoutingSlipEvents events, RoutingSlipEventContents contents, string activityName, Func<ISendEndpoint, Task> callback) where TEvent : class;
     }
 }
--- Miles.MassTransit/Courier/RoutingSlipPlanner.cs
+++ Miles.MassTransit/Courier/RoutingSlipPlanner.cs
@@ -1,7 +1,10 @@
-﻿using MassTransit.Courier;
+﻿using MassTransit;
+using MassTransit.Courier;
 using MassTransit.Courier.Contracts;
+using MassTransit.Internals.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Miles.MassTransit.Courier
 {
@@ -36,6 +39,30 @@ namespace Miles.MassTransit.Courier
         {
             var hostAddress = hostLookup.Lookup<TArguments>();
             this.AddActivity<TArguments>(name, hostAddress, arguments);
+        }
+
+        public async Task AddSubscription<TEvent>(RoutingSlipEvents events, Func<ISendEndpoint, Task> callback) where TEvent : class
+        {
+            var hostAddress = hostLookup.Lookup<TEvent>();
+            var typeName = typeof(TEvent).GetTypeName();
+            var address = new Uri(hostAddress, typeName);
+            await AddSubscription(address, events, callback);
+        }
+
+        public async Task AddSubscription<TEvent>(RoutingSlipEvents events, RoutingSlipEventContents contents, Func<ISendEndpoint, Task> callback) where TEvent : class
+        {
+            var hostAddress = hostLookup.Lookup<TEvent>();
+            var typeName = typeof(TEvent).GetTypeName();
+            var address = new Uri(hostAddress, typeName);
+            await AddSubscription(address, events, contents, callback);
+        }
+
+        public async Task AddSubscription<TEvent>(RoutingSlipEvents events, RoutingSlipEventContents contents, string activityName, Func<ISendEndpoint, Task> callback) where TEvent : class
+        {
+            var hostAddress = hostLookup.Lookup<TEvent>();
+            var typeName = typeof(TEvent).GetTypeName();
+            var address = new Uri(hostAddress, typeName);
+            await AddSubscription(address, events, contents, activityName, callback);
         }
     }
 }
