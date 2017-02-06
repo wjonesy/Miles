5776780da5ada259daeda556f8d12d21de161bcf
Adam Burton <adz21c@gmail.com>
2017-02-06 22:31:58 +0000

    EF Transaction context.

---
+++ Miles.MassTransit.EntityFramework/EFTransactionContext.cs
@@ -0,0 +1,59 @@
+﻿using Miles.Persistence;
+using System.Data;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace Miles.MassTransit.EntityFramework
+{
+    public class EFTransactionContext : SimulateNestedTransactionContext
+    {
+        private readonly DbContext dbContext;
+        private DbContextTransaction transaction = null;
+
+        public EFTransactionContext(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        protected override Task DoBeginAsync(IsolationLevel? hintIsolationLevel = null)
+        {
+            if (hintIsolationLevel.HasValue)
+                transaction = dbContext.Database.BeginTransaction(hintIsolationLevel.Value);
+            else
+                transaction = dbContext.Database.BeginTransaction();
+
+            return Task.FromResult(0);
+        }
+
+        protected override Task DoCommitAsync()
+        {
+            if (transaction != null)
+            {
+                transaction.Commit();
+                transaction = null;
+            }
+
+            return Task.FromResult(0);
+        }
+
+        protected override void DoDispose()
+        {
+            if (transaction != null)
+            {
+                transaction.Rollback();
+                transaction = null;
+            }
+        }
+
+        protected override Task DoRollbackAsync()
+        {
+            if (transaction != null)
+            {
+                transaction.Rollback();
+                transaction = null;
+            }
+
+            return Task.FromResult(0);
+        }
+    }
+}
